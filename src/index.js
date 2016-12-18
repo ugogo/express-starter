0a2c18840d4c579a234cd6d2775da452a8f3f722
onaliugo <hello@onali-ugo.fr>
2016-12-18 15:54:32 +0100

    Init app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+
+const app = express();
+
+app.server = http.createServer(app);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.server.listen(process.env.SERVER_PORT, () => {
+  console.log(`[App] Launched on http://localhost:${app.server.address().port}`);
+});
+
+export default app;
